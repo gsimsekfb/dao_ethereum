e0174a49bf71e30c1290c01752e5faa1ec1f5c17
Gökhan Şimşek <g.simsek@paralect.com>
2023-10-05 13:57:11 +0300

    Add proposalNames feature

--- contracts/dao.sol
+++ contracts/dao.sol
@@ -4,20 +4,21 @@ pragma solidity 0.8.18;
 
 contract DAO {
 
-    // Create an enum named Vote containing possible options for a vote
     enum Vote { NO, YES }
 
     // Create a mapping of ID => Proposal
     mapping(uint256 => Proposal) public proposals;
-    // todo
     mapping(uint256 => string) public proposalNames;
     // Number of proposals that have been created
     uint256 public numProposals;
+
+
     
     // --------------------------------
 
     // Create a struct named Proposal containing all relevant information
     struct Proposal {
+        // mapping(address => bool) voted;
         // deadline - the UNIX timestamp until which this proposal is active. 
         // Proposal can be executed after the deadline has been exceeded.
         // uint256 deadline;
@@ -30,10 +31,11 @@ contract DAO {
         // bool executed;
     }
 
-    function createProposal() public {
+    function createProposal(string memory name) public {
         // Set the proposal's voting deadline to be (current time + 5 minutes)
         // proposal.deadline = block.timestamp + 5 minutes;
         proposals[numProposals] = (Proposal( { yesVotes: 0, noVotes: 0 } ));
+        proposalNames[numProposals] = name;
         numProposals++;
         // return numProposals - 1;
     }
