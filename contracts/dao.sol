7ac3f5bb7f7e617f4a3963f1208812454e5ea716
Gökhan Şimşek <g.simsek@paralect.com>
2023-11-03 10:50:20 +0300

    Minor changes

--- contracts/dao.sol
+++ contracts/dao.sol
@@ -10,32 +10,28 @@ contract Dao {
 
     enum Vote { NO, YES }
 
-    // Create a mapping of ID => Proposal
+    // Create a mapping of proposal ID => Proposal
     mapping(uint256 => Proposal) public proposals;
+
     // Number of proposals that have been created
     uint256 public num_proposals;
     
-    //  Todo
+    // todo
     // mapping(address => bool) []  // array index is proposal ID
     // or better use C++ std::set e.g. [proposal ID, hash_set of addresses)], 
     // we do not really need bools
     // proposal ID => (voter_addr => voted)
     mapping(uint256 => mapping(address => bool)) public voters;
-    uint256[] public arr;
 
-    // -------------
-
+    // todo: move to Proposal.sol
     // Create a struct named Proposal containing all relevant information
     struct Proposal {
         string name;
         uint256 yes_votes;
         uint256 no_votes;                
-        uint256 deadline; // the UNIX timestamp until which this proposal is open to voting 
+        uint256 deadline; // the UNIX timestamp until which this proposal 
+                          // is open to voting 
     }
-
-    // function get_name(uint256 index) public view returns(Proposal) {
-	// 	return proposals[index];
-	// }
 
     function create_proposal(string memory name) public {
         Proposal storage proposal = proposals[num_proposals];
@@ -47,7 +43,7 @@ contract Dao {
     // hint: https://www.unixtimestamp.com/
     function change_proposal_deadline(uint256 prop_id, uint256 unix_timestamp) public {
         Proposal storage proposal = proposals[prop_id];
-        require(unix_timestamp > block.timestamp, "DEADLINE_IN_THE_PAST");
+        require(unix_timestamp > block.timestamp, "DEADLINE_CANNOT_BE_IN_THE_PAST");
         proposal.deadline = unix_timestamp;
     }
 
